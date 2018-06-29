e1ac2a191832478bb47fa02299363035afa293d5
Henry Spivey <spivotron@gmail.com>
2018-06-28 23:02:59 -0700

    updated index.js

--- todolist/src/index.js
+++ todolist/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import TodoList from './TodoList';
 
-ReactDOM.render(<App />, document.getElementById('container'));
+ReactDOM.render(<TodoList />, document.getElementById('container'));
 registerServiceWorker();
