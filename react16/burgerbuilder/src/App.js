ab5b3b3b2d985c17abb70d4d9048e4d6328f9111
Henry Spivey <spivotron@gmail.com>
2018-09-10 22:38:27 -0700

    stopped at modules

--- react16/burgerbuilder/src/App.js
+++ react16/burgerbuilder/src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import Person from './Person/Person'; // can leave off the .js
 import './App.css';
 
+
 class App extends Component {
   // any change in state will automatically invoke re render
   state = {
@@ -23,15 +24,34 @@ class App extends Component {
     ]})  // merges new state with the old one. LEAVES OTHER STUFF UNTOUCHED
   }
 
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons: [
+        {name: 'Max', age: 28},
+        {name: event.target.value, age: 24},
+      ]
+    })
+  }
+
+
   render() {
+
+    const style= {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
     return (
       <div className="App">
         <h1>Hi Im a react app </h1>
-                       // passes a reference IMPORTANT don't use ()
-        <button onClick={this.switchNameHandler.bind(this, "test1")}>Switch Name</button>
+                              // passes a reference IMPORTANT don't use ()
+        <button style={style} onClick={() => {this.switchNameHandler("test1")}}>Switch Name</button>
         // <Person name="Henry" age="4">My hobbies: Cooking</Person>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} click={this.switchNameHandler.bind(this, "test2")}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} changed={this.nameChangedHandler.bind(this)}/>
       </div>
       // gets compiled to return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'hello!!!!'));
     )
--- react16/burgerbuilder/src/Person/Person.js
+++ react16/burgerbuilder/src/Person/Person.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import './Person.css';
 //  best way to make a component, clear about what they do, don't manipulate app state, dynamic
 const person = (props) => {
   // props is an obj, passed from outside
   // state is like props, but passed from inside
   // transformed to React.createElement so we need to import React
   return (
-    <div>
+    <div className="Person">
       <p onClick={props.click}>My name is {props.name} and I am {props.age}</p>
       <p>{props.children}</p>
+      <input type="text" onChange={props.changed} value={props.name}/>
     </div>
   )
 }
