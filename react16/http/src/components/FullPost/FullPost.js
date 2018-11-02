a2ee1053342c46260a59c53b84e03b1567e6dab5
Henry Spivey <henry@webenertia.com>
2018-11-02 11:05:29 -0700

    stopped with axios

--- react16/http/src/components/FullPost/FullPost.js
+++ react16/http/src/components/FullPost/FullPost.js
@@ -1,20 +1,49 @@
 import React, { Component } from 'react';
-
+import axios from 'axios';
 import './FullPost.css';
 
 class FullPost extends Component {
+
+    state = {
+      loadedPost : null
+    }
+
+    componentDidUpdate() {
+      if (this.props.id) {
+          if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)) {
+            // only make the request for new posts
+            axios.get('/posts/'+this.props.id).then(response => {
+                this.setState({loadedPost: response.data});
+            })
+          }
+
+      }
+
+    }
+
+    deletePostHandler = () => {
+      axios.delete('/posts/'+this.props.id).then(response => {
+          console.log(response)
+      })
+    }
+
     render () {
         let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+        if (this.props.id) {
+          post = <p>Loading</p>
+        }
+        if (this.state.loadedPost) {
+          post = (
+              <div className="FullPost">
+                  <h1>{this.state.loadedPost.title}</h1>
+                  <p>{this.state.loadedPost.body}</p>
+                  <div className="Edit">
+                      <button className="Delete" onClick={this.deletePostHandler}>Delete</button>
+                  </div>
+              </div>
 
-        );
+          );
+        }
         return post;
     }
 }
--- react16/http/src/containers/Blog/Blog.js
+++ react16/http/src/containers/Blog/Blog.js
@@ -1,28 +1,45 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import axios from '../../axios';
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+      posts: [],
+      selectedPostId: null
+    }
 
     componentDidMount() {
       axios.get('http://jsonplaceholder.typicode.com/posts').then(response => {
-        console.log(response)
+        const posts = response.data.slice(0,4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post, author: 'Henri'
+          }
+        })
+        this.setState({posts: updatedPosts});
+      }).catch(error => {
+        console.log(error);
       })
     }
 
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id})
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+          return <Post key={post.id} author={post.author} title={post.title} clicked={()=> this.postSelectedHandler(post.id)} />
+        })
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
                     <NewPost />
