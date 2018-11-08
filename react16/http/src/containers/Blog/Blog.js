e9b966e63c2ac43951a3b0d28c1676dc5faf1c41
Henry Spivey <henry@webenertia.com>
2018-11-07 16:09:41 -0800

    set up active link styling

--- react16/http/src/containers/Blog/Blog.js
+++ react16/http/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import axios from '../../axios';
-import {Route, Link} from 'react-router-dom';
+import {Route, Link, NavLink} from 'react-router-dom';
 
 import './Blog.css';
 import Posts from './Posts/Posts';
@@ -14,8 +14,8 @@ class Blog extends Component {
                 <header>
                   <nav>
                     <ul>
-                      <li><Link to="/">Home</Link></li>
-                      <li><Link to="/new">New Post</Link></li>
+                      <li><NavLink to="/" exact>Home</NavLink></li>
+                      <li><NavLink to="/new">New Post</NavLink></li>
                     </ul>
                   </nav>
                 </header>
