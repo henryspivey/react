cd5f1d03dbf2cfad40d02c20cb82a33915b746f7
Henry Spivey <spivotron@gmail.com>
2018-11-20 22:15:48 -0800

    finished routing

--- react16/burgerbuilder/builder/src/App.js
+++ react16/burgerbuilder/builder/src/App.js
@@ -3,6 +3,7 @@ import Layout from './components/Layout/Layout';
 import {Route, Switch} from 'react-router-dom';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
+import Orders from './containers/Orders/Orders';
 
 class App extends Component {
   render() {
@@ -11,7 +12,9 @@ class App extends Component {
         <Layout>
         <Switch>
           <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" exact component={Orders} />
           <Route path="/" exact component={BurgerBuilder} />
+
 
         </Switch>          
         </Layout>
--- react16/burgerbuilder/builder/src/components/Navigation/NavigationItems/NavigationItems.js
+++ react16/burgerbuilder/builder/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,8 +3,8 @@ import classes from './NavigationItems.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = () => (
   <ul className={classes.NavigationItems}>
-    <NavigationItem link="/" active>Burger Buildre</NavigationItem>
-    <NavigationItem link="/">Checkout</NavigationItem>
+    <NavigationItem link="/">Burger Buildre</NavigationItem>
+    <NavigationItem link="/orders">orders</NavigationItem>
   </ul>
 );
 
