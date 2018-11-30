d68da24f16950748479a30c8e1b43df7de3fc825
Henry Spivey <spivotron@gmail.com>
2018-11-29 23:28:34 -0800

    added redux and react-redux to the project

--- react16/burgerbuilder/builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ react16/burgerbuilder/builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -32,11 +32,11 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
 
-    axios.get('https://react-my-burger-60839.firebaseio.com/ingredients.json').then(response => {
-      this.setState({ingredients: response.data})
-    }).catch(error => {
-      this.setState({error: true})
-    })
+    // axios.get('https://react-my-burger-60839.firebaseio.com/ingredients.json').then(response => {
+    //   this.setState({ingredients: response.data})
+    // }).catch(error => {
+    //   this.setState({error: true})
+    // })
   }
 
   updatePurchaseState(_ingredients) {
